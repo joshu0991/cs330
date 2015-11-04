ccf88c027f7280b5ed1cc05a6b4ccb38d5b11a64
josh <joshualilly@me.com>
2015-11-04 01:11:38 -0500

    added 2nd rule... maybe works...

--- facts.pl
+++ facts.pl
@@ -1,5 +1,8 @@
-approved_list(cs,[cs210,cs330,cs450,cs480,phys211,phys212]).
+approved_list(cs,[cs211,cs330,cs450,cs480,phys211,phys212]).
 approved_list(geog,[geol101,geog309,eos310,eos312,math113]).
+
+before(spring,summer).
+before(summer,fall).
 
 prereq(cs211,cs330).
 prereq(cs330,cs450).
@@ -11,8 +14,10 @@ prereq(geol101,geol309).
 prereq(eos310,eos312).
 
 student(bob,12345,cs).
+student(john,54321,geog).
     entry(12345,cs330,[fall,2008]).
     entry(12345,cs211,[summer,2008]).
+    entry(12345,geol101,[summer,2008]).
 
-student(John,54321,geog).
     entry(54321,geog101,[spring,2008]).
+    entry(54321,cs330,[spring,2008]).
--- rules.pl
+++ rules.pl
@@ -1,10 +1,15 @@
-prereq_chain(X,Y):- prereq(X,Y).
-prereq_chain(X, Y):- prereq(X,Z).
+prereq_chain(X,Y) :- prereq(X,Y).
+prereq_chain(X, Y) :- prereq(X,Z).
  
-bad_course(X,Y).
+bad_course(X,Y) :- student(X,N,M), entry(N,Y,_), approved_list(M,L), member(Y,L), !,fail.
+bad_course(X,Y) :- student(X,N,M), entry(N,Y,_).
 
 no_bad_courses(X).
 
 missing_course(X,Y).
 
 notbefore([S1,Y1], [S2,Y2]).
+
+member(Element, [Element|_]).
+member(Element,[_|Tail]) :-
+    member(Element,Tail).
