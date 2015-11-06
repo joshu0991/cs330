46721745aad2ee960eb814108e61b140584b0d52
josh <joshualilly@me.com>
2015-11-05 22:41:23 -0500

    added rules

--- rules.pl
+++ rules.pl
@@ -19,8 +19,8 @@ missing_course(X,Y) :- student(X,N,M), entry(N,C,_), not(Y = C), prereq_chain(Y,
 missing_course(X,Y) :- student(X,N,M), prereq_chain(Y,Q), entry(N,Z,_), entry(N,Y,_), !, fail.
 missing_course(X,Y) :- prereq_chain(Y,Z).
 
-notbefore([S1,Y1], [S2,Y2]) sesson_before(S1, S2), Y1<Y2.
-
+notbefore([S1,Y1], [S2,Y2]) :- sesson_before(S2, S1), Y1>=Y2.
+notbefore([_,Y1],[_,Y2]) :- Y1>Y2.
 sesson_before(X,Y) :- before(X,Y).
 sesson_before(X,Y) :- before(X,Z), sesson_before(Z,Y).
 
